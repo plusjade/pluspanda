ee734f7ecdcc9bb4006320ea2187f3fa1f9c661d
plusjade <plusjade@gmail.com>
2010-08-14 23:22:36 -0700

    widget_script_url helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,11 @@ module ApplicationHelper
 
   def embed_code(apikey, jquery=true)
     jquery = (jquery) ? '' : '&jquery=false'
-    code = "<div id=\"plusPandaYes\"></div><script type=\"text/javascript\" src=\"#{root_url}testimonials/widget.js?apikey=#{apikey}#{jquery}\" charset=\"utf-8\"></script>"
-    return code 
+    return "<div id=\"plusPandaYes\"></div><script type=\"text/javascript\" src=\"#{widget_script_url}#{jquery}\" charset=\"utf-8\"></script>"
+  end
+  
+  def widget_script_url
+    return "#{root_url}testimonials/widget.js?apikey=#{@user.apikey}"
   end
 
 end
