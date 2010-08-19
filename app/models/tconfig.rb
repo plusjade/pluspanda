6f2a62d9ee5aac30a6f4a1d921451bb3899462dc
plusjade <plusjade@gmail.com>
2010-08-19 03:32:43 -0700

    add default form hash and validate

--- app/models/tconfig.rb
+++ app/models/tconfig.rb
@@ -1,15 +1,28 @@
 class Tconfig < ActiveRecord::Base
   serialize :form, Hash
   belongs_to :user
-  before_save {self.form = self.form.inject({}) { |h,(k,v)| h[k] = v.strip ; h }}
-  # No clue why this does not work: (because of the yamlesq Hash difference?)
-    #before_save { self.form = self.form.merge(self.form){ |k,ov| ov.strip } }
   before_create :generate_defaults
-  
+  before_save   :format
+
+
   def generate_defaults 
     self.theme     ||= 'list'
     self.sort      ||= 'created'
-    self.per_page  ||= 10    
+    self.per_page  ||= 10 
+    self.form      ||= {
+      "meta"        => "",
+      "require_key" => "",
+      #"email"       => ""
+    }
+  end
+
+
+  # No clue why this does not work: (because of the yamlesq Hash difference?)
+    #before_save { self.form = self.form.merge(self.form){ |k,ov| ov.strip } }
+  def format
+    if self.form.is_a?(Hash)
+      self.form = self.form.inject({}) { |h,(k,v)| h[k] = v.strip ; h }
+    end
   end
       
 end
