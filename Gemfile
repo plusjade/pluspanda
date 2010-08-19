c92492c24ab07be06c4a7e667115981808df11a7
plusjade <plusjade@gmail.com>
2010-08-18 22:09:31 -0700

    add sanitize gem to sanitize testimonial input

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.0.rc'
+gem "nokogiri"
+gem "sanitize"
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,16 +1,33 @@
+require 'sanitize'
 class Testimonial < ActiveRecord::Base
 
   has_one :tag
   
   before_create :generate_defaults
-  
+  before_save :sanitize
+
+  SANITIZE_CONFIG  = {
+    :elements => [
+      'div','span','br','h1','h2','h3','h4','h5','h6','p','blockquote','pre','a','abbr','acronym','address','big','cite','code','del','dfn','em','font','img','ins','kbd','q','s','samp','small','strike','strong','sub','sup','tt','var','b','u','i','center','dl','dt','dd','ol','ul','li','table','caption','tbody','tfoot','thead','tr','th','td'
+    ],
+    :attributes => {
+      :all  => ['id','class', 'title','rel','style'],
+      'a'   => ['href'],
+      'img' => ['src','alt']
+    }
+  }  
+    
   def generate_defaults 
     self.token = ActiveSupport::SecureRandom.hex(6)
   end
   
-  def blah
+  def sanitize
+    self.body       = Sanitize.clean(self.body, SANITIZE_CONFIG) 
+    self.company    = Sanitize.clean(self.company)
+    self.c_position = Sanitize.clean(self.c_position)
+    self.location   = Sanitize.clean(self.location)
+    self.url        = Sanitize.clean(self.url)
     #self.url gsub!('http://','', strtolower($this->url));
-    
   end
   
     
