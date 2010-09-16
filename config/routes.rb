a2e014ee51a1e7e3b84b9d2cde4b2d2b4b516794
plusjade <plusjade@gmail.com>
2010-09-16 02:16:31 -0700

    removed marketing pages and put on its own sinatra app @heroku

--- config/routes.rb
+++ config/routes.rb
@@ -48,13 +48,7 @@ Pluspanda::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to                  => "marketing#index"
-  match 'home'              => 'marketing#index'
-  match 'pricing'           => 'marketing#pricing'
-  match 'faq'               => 'marketing#faq'
-  match 'contact'           => 'marketing#contact'
-  match 'terms_of_service'  => 'marketing#terms_of_service'
-  match 'privacy_policy'    => 'marketing#privacy_policy'
+  #root :to                  => "marketing#index"
   
   match 'admin'             => 'admin#index'
   match 'admin/manage'      => 'admin#manage'
