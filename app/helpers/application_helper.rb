9a35609e6091608bb14de27f6f24a663e59f28c6
plusjade <plusjade@gmail.com>
2010-09-19 02:20:21 -0700

    update install code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
 
-  def embed_code(apikey, jquery=true)
-    jquery = (jquery) ? '' : '&jquery=false'
-    return "<div id=\"plusPandaYes\"></div><script type=\"text/javascript\" src=\"#{widget_script_url}#{jquery}\" charset=\"utf-8\"></script>"
+  def embed_code(apikey)
+    return "<script type=\"text/javascript\" src=\"#{widget_script_url}\" charset=\"utf-8\"></script>"
   end
   
   def widget_script_url
