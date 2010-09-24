104bcebcd65d3699b66877b93d3c6469bad59412
plusjade <plusjade@gmail.com>
2010-09-24 16:21:19 -0700

    send emails via sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Pluspanda::Application.initialize!
 
-sendgrid = YAML::load(File.open("#{RAILS_ROOT}/config/sendgrid.yml"))
+sendgrid = YAML::load(File.open("#{::Rails.root.to_s}/config/sendgrid.yml"))
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
   :port => '25',
   :domain => "api.pluspanda.com",
   :authentication => :plain,
-  :user_name => sendgrid[:user_name],
-  :password => sendgrid[:password]
+  :user_name => sendgrid['user_name'],
+  :password => sendgrid['password']
 }
 ActionMailer::Base.default :content_type => "text/html"
