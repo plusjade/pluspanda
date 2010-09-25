2bb8003de028e3609a9478ab81ab10364001ebab
plusjade <plusjade@gmail.com>
2010-09-24 18:24:27 -0700

    transparently log in existing users by to new site by authenticating from old site

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -12,6 +12,27 @@ class UserSessionsController < ApplicationController
     if @user_session.save
       redirect_to admin_path
     else
+      # try to login at the old site teehee
+      email    = params[:user_session][:email]
+      password = params[:user_session][:password]
+      response = Net::HTTP.post_form(URI.parse('http://old.pluspanda.com/admin/login'),
+                                    {'email'=> email , 'password'=> password })
+      r        = ActiveSupport::JSON.decode(response.body)
+      
+      if r['status'] == 'good'
+        user = User.find_by_email(email)
+        puts user
+        user.password = password
+        user.password_confirmation = password
+        if user.save
+          if @user_session.save
+            redirect_to admin_path
+            return
+          end
+        end
+        puts user.errors
+      end
+      
       flash[:notice] = "Incorrect login information"
       render :action => :new
     end
