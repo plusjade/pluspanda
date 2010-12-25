a2d1cb5574d7d145251a6869ffd4d480ce9ed972
plusjade <plusjade@gmail.com>
2010-12-25 03:00:32 -0800

    sanitize HTML input

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -17,7 +17,7 @@ class Testimonial < ActiveRecord::Base
       'div','span','br','h1','h2','h3','h4','h5','h6','p','blockquote','pre','a','abbr','acronym','address','big','cite','code','del','dfn','em','font','img','ins','kbd','q','s','samp','small','strike','strong','sub','sup','tt','var','b','u','i','center','dl','dt','dd','ol','ul','li','table','caption','tbody','tfoot','thead','tr','th','td'
     ],
     :attributes => {
-      :all  => ['id','class', 'title','rel','style'],
+      :all  => ['id','class','title','rel','style'],
       'a'   => ['href'],
       'img' => ['src','alt']
     }
--- app/models/theme_attribute.rb
+++ app/models/theme_attribute.rb
@@ -1,7 +1,20 @@
+require 'sanitize'
 class ThemeAttribute < ActiveRecord::Base
 
   belongs_to :theme
-
+  before_save :sanitize
+  
+  SANITIZE_CONFIG  = {
+    :elements => [
+      'div','span','br','h1','h2','h3','h4','h5','h6','p','blockquote','pre','a','abbr','acronym','address','big','cite','code','del','dfn','em','font','img','ins','kbd','q','s','samp','small','strike','strong','sub','sup','tt','var','b','u','i','center','dl','dt','dd','ol','ul','li','table','caption','tbody','tfoot','thead','tr','th','td'
+    ],
+    :attributes => {
+      :all  => ['id','class','title','rel','style'],
+      'a'   => ['href'],
+      'img' => ['src','alt']
+    }
+  }
+  
   def self.names
     [
       "wrapper.html",
@@ -11,4 +24,12 @@ class ThemeAttribute < ActiveRecord::Base
     ]
   end
 
+
+  def sanitize
+    unless self.name == 2
+      self.staged = Sanitize.clean(self.staged.to_s, SANITIZE_CONFIG)
+    end
+  end
+  
+  
 end
