d3976f29e9b9e6fd10d4902833d3a0bdeacc21ef
plusjade <plusjade@gmail.com>
2011-02-26 20:33:03 -0800

    remove checking out old pluspanda database

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -13,28 +13,7 @@ class UserSessionsController < ApplicationController
       @user_session.record.update_attributes(:last_login_at => Time.now)
       redirect_to admin_path
     else
-      # try to login at the old site teehee
-      email    = params[:user_session][:email]
-      password = params[:user_session][:password]
-      response = Net::HTTP.post_form(URI.parse('http://old.pluspanda.com/admin/login'),
-                                    {'email'=> email , 'password'=> password })
-      r        = ActiveSupport::JSON.decode(response.body)
-      
-      if r['status'] == 'good'
-        user = User.find_by_email(email)
-        puts user
-        user.password = password
-        user.password_confirmation = password
-        if user.save
-          if @user_session.save
-            redirect_to admin_path
-            return
-          end
-        end
-        puts user.errors
-      end
-      
-      flash[:notice] = "Incorrect login information"
+      flash[:notice] = @user_session.errors.to_a.join(", ")
       render :action => :new
     end
   end
