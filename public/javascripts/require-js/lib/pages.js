fe5f3fc63d11a05422c0eba8ce0d6d4390e322c1
Jade Dominguez <plusjade@gmail.com>
2013-03-17 14:47:34 -0700

    Upgrade CodeMirrror to 3.1

--- public/javascripts/require-js/lib/pages.js
+++ public/javascripts/require-js/lib/pages.js
@@ -13,251 +13,56 @@ define([
       if(typeof this[page] === "function")
         this[page]();
     },
-  
-  /* twitter pages */
 
-    'admin/t_manage' : function(){
-      // TODO: make this declare only once and for published filter only
-      $('#tweet_list.published').sortable({
-        items:'div.tweet',
-        axis: 'y',
-        helper: 'clone',
-        update: function(event, ui) {
-          var order = $("#tweet_list.published").sortable("serialize");
-          if(order){
-            showStatus.submitting();    
-            $.get('/twitter/tweets/save_positions', order, function(rsp){
-              showStatus.respond(rsp);
-            })
-          } 
-          else {
-            showStatus.respond({"msg":'No items to sort'});
-          }
-        }
-      });
-    
-      $("a.trash").click(function(e){
-        if(confirm("Trash this Tweet?")){
-          var $a = $(this);
-          showStatus.submitting();
-          $.get('/twitter/tweets/'+ $a.attr("rel") +'/trash', function(rsp){
-            showStatus.respond(rsp);
-            if(rsp && rsp.status === "good")
-              $a.parent().remove();
-          })
-        }
-        e.preventDefault();
-        return false;
-      })
-    
-      $("ul.sub-tabs li a").click(function(){
-        adminNavigation.subTab($(this));
-        return false;
-      });
-    
-      $(document).trigger('ajaxify.form');
-    },
-  
-    'admin/t_widget' : function(){
-      var editor = {}
-       editor.wrapper = window.CodeMirror.fromTextArea('editor_wrapper', {
-         width: "850px",
-         height: "700px",
-         parserfile: "parsexml.js",
-         stylesheet: "/stylesheets/codemirror/xmlcolors.css?1",
-         path: "/javascripts/codemirror/",
-         continuousScanning: 500,
-         lineNumbers: true,
-         textWrapping: false,
-         saveFunction: function(){
-           $('#editor_wrapper').val(editor.wrapper.getCode());
-           $('#wrapper-form').submit();
-           mpmetrics.track("editor save:widget");
-         },
-         initCallback: function(editor){
-           //editor.setCode('some value');    
-         }    
-       });
-
-       editor.testimonial = window.CodeMirror.fromTextArea('editor_testimonial', {
-         width: "850px",
-         height: "700px",
-         parserfile: "parsexml.js",
-         stylesheet: "/stylesheets/codemirror/xmlcolors.css?3453",
-         path: "/javascripts/codemirror/",
-         continuousScanning: 500,
-         lineNumbers: true,
-         textWrapping: false,
-         saveFunction: function(){
-           $('#editor_testimonial').val(editor.testimonial.getCode());
-           $('#testimonial-form').submit();
-           mpmetrics.track("editor save:testimonial");
-         },
-         initCallback: function(editor){
-           //editor.setCode('some value');    
-         }    
-       });
-
-       editor.css = window.CodeMirror.fromTextArea('editor_css', {
-         width: "850px",
-         height: "700px",
-         parserfile: "parsecss.js",
-         stylesheet: "/stylesheets/codemirror/csscolors.css?3453",
-         path: "/javascripts/codemirror/",
-         continuousScanning: 500,
-         lineNumbers: true,
-         textWrapping: false,
-         saveFunction: function(){
-           $('#editor_css').val(editor.css.getCode());
-           $('#css-form').submit();
-           mpmetrics.track("editor save:css");
-         },
-         initCallback: function(editor){
-           //editor.setCode('some value');    
-         }    
-       });
-
-
-      // overload save button for saving data
-       $('#wrapper-form button').click(function(){
-         $('#editor_wrapper').val(editor.wrapper.getCode());
-       });
-       $('#testimonial-form button').click(function(){
-         $('#editor_testimonial').val(editor.testimonial.getCode());
-       });
-       $('#css-form button').click(function(){
-         $('#editor_css').val(editor.css.getCode());
-       });
-
-       // editor data-loading functions
-       $('#load-stock-wrapper, #refresh-wrapper').click(function(){
-         showStatus.submitting();
-         $.get(this.href, {rand: Math.random()}, function(data){
-           editor.wrapper.setCode(data);
-           showStatus.respond({status:'good', msg:'Wrapper HTML Loaded.'});
-         });
-         return false;
-       })
-       $('#load-stock-testimonial, #refresh-testimonial').click(function(){
-         showStatus.submitting();
-         $.get(this.href, {rand: Math.random()}, function(data){
-           editor.testimonial.setCode(data);
-           showStatus.respond({status:'good', msg:'HTML Loaded.'});
-         });
-         return false;
-       })
-       $('#load-stock-css, #refresh-css').click(function(){
-         showStatus.submitting();
-         $.get(this.href, {rand: Math.random()}, function(data){
-           editor.css.setCode(data);
-           showStatus.respond({status:'good', msg:'CSS Loaded.'});
-         });
-         return false;
-       })    
-
-
-       $("#theme-publish").click(function(){
-         showStatus.submitting();
-         $.get(this.href, function(rsp){
-           showStatus.respond(rsp);
-         })
-         return false;
-       });
-
-       // subtabs
-       $("ul.sub-tabs li a").click(function(){
-         adminNavigation.subTab($(this));
-
-         var tab = $(this).attr("href").substring(1);
-         switch(tab){
-           case "published":
-             twitterWidget.loadWidgetPublished();
-             break;
-           case "staged":
-             twitterWidget.loadWidgetStaged();
-             break;
-         }
-
-         return false;
-       });
-
-       $("#installed-themes li.theme").find("a").click(function(){
-         showStatus.submitting();
-         $.get(this.href, function(rsp){
-           showStatus.respond(rsp);
-           sammyApp.refresh();
-         })
-         return false;
-       });
-
-       // init
-       twitterWidget.loadWidgetPublished();
-       adminNavigation.initSubs();
-       $(document).trigger('ajaxify.form');
-    
-    },
-  
-  /* testimonial pages */ 
- 
     'admin/widget' : function(){
       var editor = {}
-      editor.wrapper = window.CodeMirror.fromTextArea('editor_wrapper', {
-        width: "850px",
-        height: "700px",
-        parserfile: "parsexml.js",
-        stylesheet: "/stylesheets/codemirror/xmlcolors.css?1",
-        path: "/javascripts/codemirror/",
+      editor.wrapper = window.CodeMirror.fromTextArea(document.getElementById("editor_wrapper"), {
+        mode: { name: "xml", htmlMode: true },
+        theme: "ambiance",
+        path: "/codemirror-3.1/",
         continuousScanning: 500,
-        lineNumbers: true,
-        textWrapping: false,
-        saveFunction: function(){
-          $('#editor_wrapper').val(editor.wrapper.getCode());
-          $('#wrapper-form').submit();
-          mpmetrics.track("editor save:widget");
-        },
-        initCallback: function(editor){
-          //editor.setCode('some value');    
-        }    
+        lineNumbers: true
       });
+      editor.wrapper.addKeyMap({
+        'Cmd-S' : function(cm){
+            $('#editor_wrapper').val(cm.getValue());
+            $('#wrapper-form').submit();
+            mpmetrics.track("editor save:widget");
+        }
+      })
 
-      editor.testimonial = window.CodeMirror.fromTextArea('editor_testimonial', {
-        width: "850px",
-        height: "700px",
-        parserfile: "parsexml.js",
-        stylesheet: "/stylesheets/codemirror/xmlcolors.css?3453",
-        path: "/javascripts/codemirror/",
+      editor.testimonial = window.CodeMirror.fromTextArea(document.getElementById('editor_testimonial'), {
+        mode: { name: "xml", htmlMode: true },
+        theme: "ambiance",
         continuousScanning: 500,
         lineNumbers: true,
-        textWrapping: false,
-        saveFunction: function(){
-          $('#editor_testimonial').val(editor.testimonial.getCode());
-          $('#testimonial-form').submit();
-          mpmetrics.track("editor save:testimonial");
-        },
-        initCallback: function(editor){
-          //editor.setCode('some value');    
-        }    
+        textWrapping: false
       });
+      editor.wrapper.addKeyMap({
+        'Cmd-S' : function(cm){
+            $('#editor_testimonial').val(cm.getValue());
+            $('#testimonial-form').submit();
+            mpmetrics.track("editor save:testimonial");
+        }
+      })
 
-      editor.css = window.CodeMirror.fromTextArea('editor_css', {
+      editor.css = window.CodeMirror.fromTextArea(document.getElementById('editor_css'), {
         width: "850px",
         height: "700px",
-        parserfile: "parsecss.js",
-        stylesheet: "/stylesheets/codemirror/csscolors.css?3453",
-        path: "/javascripts/codemirror/",
+        mode: { name: "css"},
+        theme: "ambiance",
         continuousScanning: 500,
         lineNumbers: true,
-        textWrapping: false,
-        saveFunction: function(){
-          $('#editor_css').val(editor.css.getCode());
-          $('#css-form').submit();
-          mpmetrics.track("editor save:css");
-        },
-        initCallback: function(editor){
-          //editor.setCode('some value');    
-        }    
+        textWrapping: false
       });
-            
+      editor.css.addKeyMap({
+        'Cmd-S' : function(cm){
+            $('#editor_css').val(cm.getValue());
+            $('#css-form').submit();
+            mpmetrics.track("editor save:css");
+        }
+      })
+    
      // overload save button for saving data
       $('#wrapper-form button').click(function(){
         $('#editor_wrapper').val(editor.wrapper.getCode());
@@ -268,12 +73,12 @@ define([
       $('#css-form button').click(function(){
         $('#editor_css').val(editor.css.getCode());
       });
-       
+
       // editor data-loading functions
       $('#load-stock-wrapper, #refresh-wrapper').click(function(){
         showStatus.submitting();
         $.get(this.href, {rand: Math.random()}, function(data){
-          editor.wrapper.setCode(data);
+          editor.wrapper.setValue(data);
           showStatus.respond({status:'good', msg:'Wrapper HTML Loaded.'});
         });
         return false;
@@ -281,7 +86,7 @@ define([
       $('#load-stock-testimonial, #refresh-testimonial').click(function(){
         showStatus.submitting();
         $.get(this.href, {rand: Math.random()}, function(data){
-          editor.testimonial.setCode(data);
+          editor.testimonial.setValue(data);
           showStatus.respond({status:'good', msg:'Testimonial HTML Loaded.'});
         });
         return false;
@@ -289,7 +94,7 @@ define([
       $('#load-stock-css, #refresh-css').click(function(){
         showStatus.submitting();
         $.get(this.href, {rand: Math.random()}, function(data){
-          editor.css.setCode(data);
+          editor.css.setValue(data);
           showStatus.respond({status:'good', msg:'CSS Loaded.'});
         });
         return false;
@@ -356,7 +161,7 @@ define([
       });
 
       // subtabs
-      $("ul.sub-tabs li a").click(function(){
+      $(".manage-testimonial-filters").find('a').click(function(){
         $target = $(this);
         $(".data-description").html($target.attr('title'));
         var filter = $target.html().toLowerCase();
