ec89bb7e387bc97c3e4cb11ad5ba14545d4f3ae6
Jade Dominguez <plusjade@gmail.com>
2013-03-21 20:26:36 -0700

    Add back in facebox support since manage page still uses it.

--- public/javascripts/require-js/app.js
+++ public/javascripts/require-js/app.js
@@ -43,6 +43,15 @@ define([
     },
 
     bind : function(){
+        $("a[rel*=facebox]").live("click", function(){
+            var url = this.href
+            $.facebox(function(){ 
+              $.get(url, function(data) { $.facebox(data) })
+            })
+            mpmetrics.track(url);
+            return false;    
+          });
+
       $("a[rel*=fb-div]").live("click", function(){
         $.facebox({div : this.href});
         mpmetrics.track(this.href);
