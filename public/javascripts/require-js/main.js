3288184f677867c901b341bc2722ab0520f12e64
Jade Dominguez <plusjade@gmail.com>
2013-11-03 17:06:52 -0800

    Update backbone and underscore.

--- public/javascripts/require-js/main.js
+++ public/javascripts/require-js/main.js
@@ -1,14 +1,23 @@
 require.config({
   baseUrl : "/javascripts/require-js/",
   urlArgs: "bust=" + (new Date()).getTime(),
+  shim: {
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ["underscore", "jquery"],
+      exports: "Backbone"
+    }
+  },
   paths : {
     'jquery' : 'libs/jquery-1.8.3',
-    'underscore': 'libs/underscore-1.3.1-amd', // AMD support
-    'backbone': 'libs/backbone-0.9.1-amd', // AMD support
+    'underscore': 'libs/underscore.1.5.2.min',
+    'backbone': 'libs/backbone.1.1.0.min',
     'mustache' : 'libs/mustache-0.7.2',
   }  
 });
 
-require(['app'], function(App){
-  App.start();
+require(['jquery', 'underscore', 'backbone', 'app'], function ($, _, Backbone, App) {
+    App.start();
 });
