36d333899dc1e6d77f406884bd04eb30c3490d97
Jade Dominguez <plusjade@gmail.com>
2014-02-11 21:00:15 -0800

    Refactor widget.js and upgrade to jquery 1.9 syntax.

--- public/javascripts/widget/widget.js
+++ public/javascripts/widget/widget.js
@@ -1,135 +1,425 @@
-var panda = {
-  loading: '<div class="ajax_loading">Loading...</div>',
-  $container : null,
-  $testimonials : null,
-  $iframe : null,
-  $total : null,
-  pollTry: 0,
-  admin : false,
-  debug: false,
-  
-  init: function () {
-    if (window.location.hash === '#panda.admin') panda.admin = true;
-    if (window.location.hash === '#panda.debug') panda.debug = true;
-    if (panda.debug){console.log("init called, expecting jQuery, then setup()")}
-    if (typeof jQuery === 'undefined') { 
-      if(panda.debug){console.log('no jQuery, expecting google cdn and poll(), then setup()')} 
-      var script = document.createElement("script");
-      script.type = "text/javascript";
-      script.src = "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js";    
-      document.body.appendChild(script);
-      panda.poll()
-    } else {
-      panda.setup()
+var panda = function() {
+    var loadingMessage = '<div class="ajax_loading">Loading...</div>',
+        $container,
+        $testimonials,
+        $total,
+        pollTry = 0,
+        isAdmin = false,
+        isDebug = false
+        ;
+
+    function init() {
+        if (window.location.hash === '#panda.admin') isAdmin = true;
+        if (window.location.hash === '#panda.debug') isDebug = true;
+        if (isDebug) {
+            console.log("init called, expecting jQuery, then setup()");
+        }
+
+        if (typeof jQuery === 'undefined') { 
+            if(isDebug) {
+                console.log('no jQuery, expecting google cdn and poll(), then setup()');
+            } 
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js";
+            document.body.appendChild(script);
+            poll();
+        }
+        else {
+            setup();
+        }
     }
-  },
-  
-  poll: function (){
-    panda.pollTry = panda.pollTry+1;
-    if (panda.debug){console.log("polling, expecting jQuery. Try#:" + panda.pollTry)}
-    
-    if (panda.pollTry > 25) { 
-      var container = document.getElementById('plusPandaYes');
-      container.innerHTML = 'pluspanda error! Could not load jquery';
-    } else if (typeof jQuery === 'undefined') {  
-      setTimeout("panda.poll()", 150);
-    } else {
-      panda.setup()
+
+    function poll() {
+        if (isDebug) {
+            console.log("polling, expecting jQuery. Try#:" + pollTry);
+        }
+
+        pollTry += 1;
+
+        if (pollTry > 25) {
+            var container = document.getElementById('plusPandaYes');
+            container.innerHTML = 'pluspanda error! Could not load jquery';
+        }
+        else if (typeof jQuery === 'undefined') {
+            setTimeout(function() { poll() }, 150);
+        }
+        else {
+           setup();
+        }
     }
-  }, 
 
-  setup: function(){ 
-    if(panda.debug){console.log("setup called, expecting css, $container, delegation, then getTstmls()")}
-    var cssUrl = pandaThemeConfig.cssUrl + ( panda.admin ? ('?r=' + Math.random()) : '' );
-    if(pandaThemeConfig.cssUrl.length > 0) jQuery('head').append('<link type="text/css" href="' + cssUrl + '" media="screen" rel="stylesheet" />');
+    function setup() {
+        if(isDebug) {
+            console.log("setup called, expecting css, $container, delegation, then getTestimonials()");
+        }
+        var cssUrl = pandaThemeConfig.cssUrl + ( isAdmin ? ('?r=' + Math.random()) : '' );
+        if(pandaThemeConfig.cssUrl.length > 0) {
+            jQuery('head').append('<link type="text/css" href="' + cssUrl + '" media="screen" rel="stylesheet" />');
+        }
 
-    panda.$container = jQuery('#plusPandaYes').html(pandaThemeConfig.wrapperHTML);
-    panda.$testimonials = panda.$container.find('span.pandA-tWrapper_ness');
-    panda.$totals = panda.$container.find("span.pandA-tCount_ness");
-    panda.$tags = panda.$container.find("ul.pandA-tTags_ness");
+        $container = jQuery('#plusPandaYes').html(pandaThemeConfig.wrapperHTML);
+        $testimonials = $container.find('span.pandA-tWrapper_ness');
+        $totals = $container.find("span.pandA-tCount_ness");
+        $tags = $container.find("ul.pandA-tTags_ness");
 
-    panda.$container.find("a.show-more").live("click",function(){  
-      var is_sort = this.href.indexOf('#');    
-      var parent = (-1 == is_sort) ? 'a.show-more' : '.panda-testimonials-sorters a';
-      var spltr = (-1 == is_sort) ? '?' : '#';
+        $container.on("click", 'a.show-more', function() {
+            var is_sort = this.href.indexOf('#');
+            var parent = (-1 == is_sort) ? 'a.show-more' : '.panda-testimonials-sorters a';
+            var spltr = (-1 == is_sort) ? '?' : '#';
 
-      // get GET params from links TOD0: optimize this?
-      var arr = this.href.split(spltr)[1].split('&');
-      var params = {"tag":"all","sort":"newest","page":1};
-      jQuery.each(arr, function(){
-        var pair = this.toString().split('=');
-        params[pair[0]] = pair[1]; 
-      })
-      jQuery(this).remove();
-      panda.getTstmls(params.tag, params.sort, params.page);
-      return false;
-    });
-    panda.$tags.find("a").live("click", function(){
-      var tag = this.hash.substring(1);
-      panda.$tags.find('a').removeClass('active');
-      jQuery(this).addClass('active');
+            // get GET params from links TOD0: optimize this?
+            var arr = this.href.split(spltr)[1].split('&');
+            var params = {"tag":"all","sort":"newest","page":1};
+            jQuery.each(arr, function() {
+                var pair = this.toString().split('=');
+                params[pair[0]] = pair[1]; 
+            })
 
-      panda.$testimonials.empty();
-      panda.getTstmls(tag,'newest',1);
-      return false;  
-    });
-    
-    panda.initFacebox();
+            jQuery(this).remove();
+            getTestimonials(params.tag, params.sort, params.page);
+            return false;
+        });
 
-    if(panda.debug){console.log(cssUrl);console.log(panda.$container)}
-    panda.getTstmls('all','newest',1)
-  },
+        $tags.on("click", 'a', function() {
+            var tag = this.hash.substring(1);
+            $tags.find('a').removeClass('active');
+            jQuery(this).addClass('active');
 
-  initFacebox : function(){
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.src = pandaSettings.fbUrl
-    document.body.appendChild(script);
-  },
-  
-  /* get the testimonials as json. */
-  getTstmls: function (tag, sort, page){
-    if(panda.debug){console.log("getTstmls called(" + tag + '/' + sort + '/' + page+ ')');console.log('expecting callbacks: display(), update()')}
-    panda.$testimonials.append(panda.loading);
-    jQuery.ajax({ 
-        type:'GET', 
-        url: pandaSettings.apiUrl + '/' + pandaSettings.apiVrsn + '/testimonials.js', 
-        data:"apikey="+pandaSettings.apikey+"&tag="+tag+"&sort="+sort+"&page="+page, 
-        dataType:'jsonp'
-    }); 
-  },
-  
-  clean: function (){
-    jQuery('head script[src^="' + pandaSettings.apiUrl + '"]').remove();
-  },  
-  
-  /* callback to format and inject testimonials data. */
-  display: function (tstmls){
-    if(panda.debug){console.log("display called, expecting updated content.");console.log(tstmls)}
-    var content = '';
-    jQuery(tstmls).each(function(i){
-      this.created_at = new Date(this.created_at).toDateString();
-      this.image  = (false === this.image) ? this.image_stock : this.image; 
-      this.image  = '<img src="'+ this.image +'" />';
-      if(this.url.length > 0){
-        this.url    = '<a href="http://' + this.url + '" target="_blank">http://' + this.url + ' </a>';
+            $testimonials.empty();
+            getTestimonials(tag,'newest',1);
+            return false;  
+        });
+
+        initFacebox(jQuery);
+
+        if(isDebug) {
+            console.log(cssUrl);console.log($container)
+        }
+
+        getTestimonials('all','newest',1);
+    }
+
+    // get the testimonials as json.
+    function getTestimonials(tag, sort, page) {
+        if(isDebug) {
+            console.log("getTestimonials called(" + tag + '/' + sort + '/' + page+ ')');
+            console.log('expecting callbacks: display(), update()')
+        }
+
+        $testimonials.append(loadingMessage);
+        jQuery.ajax({
+            type:'GET', 
+            url: pandaSettings.apiUrl + '/' + pandaSettings.apiVrsn + '/testimonials.js', 
+            data:"apikey="+pandaSettings.apikey+"&tag="+tag+"&sort="+sort+"&page="+page, 
+            dataType:'jsonp'
+        }); 
+    }
+
+    function clean() {
+        jQuery('head script[src^="' + pandaSettings.apiUrl + '"]').remove();
+    }
+
+    // callback to format and inject testimonials data.
+    function display(tstmls) {
+        if(isDebug) {
+            console.log("display called, expecting updated content.");console.log(tstmls)
+        }
+
+        var content = '';
+        jQuery(tstmls).each(function(i) {
+            this.created_at = new Date(this.created_at).toDateString();
+            this.image  = (false === this.image) ? this.image_stock : this.image; 
+            this.image  = '<img src="'+ this.image +'" />';
+            if(this.url.length > 0) {
+                this.url    = '<a href="http://' + this.url + '" target="_blank">http://' + this.url + ' </a>';
+            }
+            this.alt = (0 === (i+1) % 2) ? 'even' : 'odd';
+            this.tag_name = (this.tag_name)? this.tag_name : '';
+            content  += pandaThemeConfig.testimonialHTML(this);
+        });
+        if(isDebug) {
+            console.log(content)
+        }
+
+        $testimonials.find(".ajax_loading").replaceWith(content);
+        clean();
+    }
+
+    // callback to update display with pagination, counts, etc
+    function update(data) {
+        if (isDebug) {
+            console.log("update called, expecting total count and pagination updated.");
+            console.log(data);
+        }
+
+        $totals.html(data.total);
+        if(!data.nextPage) return false;
+
+        var link = '<a href="' + data.nextPageUrl + '" class="show-more">Show More</a>';
+        $testimonials.append(link);
+    }
+
+    // Facebox (for jQuery)
+    // @homepage https://github.com/defunkt/facebox
+    function initFacebox($) {
+      $.facebox = function(data, klass) {
+        $.facebox.loading()
+
+        if (data.ajax) fillFaceboxFromAjax(data.ajax, klass)
+        else if (data.image) fillFaceboxFromImage(data.image, klass)
+        else if (data.div) fillFaceboxFromHref(data.div, klass)
+        else if ($.isFunction(data)) data.call($)
+        else $.facebox.reveal(data, klass)
       }
-      this.alt        = (0 === (i+1) % 2) ? 'even' : 'odd';
-      this.tag_name   = (this.tag_name)? this.tag_name : '';
-      content  += pandaThemeConfig.testimonialHTML(this);
-    });
-    panda.$testimonials.find(".ajax_loading").replaceWith(content);
-    if(panda.debug) console.log(content);
-    panda.clean();
-  },
 
-  /* callback to update display with pagination, counts, etc */
-  update: function (data){
-    if (panda.debug){console.log("update called, expecting total count and pagination updated.");console.log(data);}
-    panda.$totals.html(data.total);
-    if(!data.nextPage) return false;
-    var link = '<a href="' + data.nextPageUrl + '" class="show-more">Show More</a>';
-    panda.$testimonials.append(link);
-  }
-}
+      /*
+       * Public, $.facebox methods
+       */
+
+      $.extend($.facebox, {
+        settings: {
+          opacity      : 0.7,
+          overlay      : true,
+          loadingImage : '/images/loading.gif',
+          closeImage   : '/images/closelabel.png',
+          imageTypes   : [ 'png', 'jpg', 'jpeg', 'gif' ],
+          faceboxHtml  : '\
+        <div id="facebox" style="display:none;"> \
+          <div class="popup"> \
+            <div class="content"> \
+            </div> \
+            <a href="#" class="close">CLOSE</a> \
+          </div> \
+        </div>'
+        },
+
+        loading: function() {
+          init()
+          if ($('#facebox .loading').length == 1) return true
+          showOverlay()
+
+          $('#facebox .content').empty()
+          $('#facebox .body').children().hide().end().
+            append('<div class="loading"><img src="'+$.facebox.settings.loadingImage+'"/></div>')
+
+          $('#facebox').css({
+            top:    getPageScroll()[1] + (getPageHeight() / 10),
+            left:   $(window).width() / 2 - 205
+          }).show()
+
+          $(document).bind('keydown.facebox', function(e) {
+            if (e.keyCode == 27) $.facebox.close()
+            return true
+          })
+          $(document).trigger('loading.facebox')
+        },
+
+        reveal: function(data, klass) {
+          $(document).trigger('beforeReveal.facebox')
+          if (klass) $('#facebox .content').addClass(klass)
+          $('#facebox .content').append(data)
+          $('#facebox .loading').remove()
+          $('#facebox .body').children().fadeIn('normal')
+          $('#facebox').css('left', $(window).width() / 2 - ($('#facebox .popup').width() / 2))
+          $(document).trigger('reveal.facebox').trigger('afterReveal.facebox')
+        },
+
+        close: function() {
+          $(document).trigger('close.facebox')
+          return false
+        }
+      })
+
+      /*
+       * Public, $.fn methods
+       */
+
+      $.fn.facebox = function(settings) {
+        if ($(this).length == 0) return
+
+        init(settings)
+
+        function clickHandler() {
+          $.facebox.loading(true)
+
+          // support for rel="facebox.inline_popup" syntax, to add a class
+          // also supports deprecated "facebox[.inline_popup]" syntax
+          var klass = this.rel.match(/facebox\[?\.(\w+)\]?/)
+          if (klass) klass = klass[1]
+
+          fillFaceboxFromHref(this.href, klass)
+          return false
+        }
+
+        return this.bind('click.facebox', clickHandler)
+      }
+
+      /*
+       * Private methods
+       */
+
+      // called one time to setup facebox on this page
+      function init(settings) {
+        if ($.facebox.settings.inited) return true
+        else $.facebox.settings.inited = true
+
+        $(document).trigger('init.facebox')
+        makeCompatible()
+
+        var imageTypes = $.facebox.settings.imageTypes.join('|')
+        $.facebox.settings.imageTypesRegexp = new RegExp('\.(' + imageTypes + ')$', 'i')
+
+        if (settings) $.extend($.facebox.settings, settings)
+        $('body').append($.facebox.settings.faceboxHtml)
+
+        var preload = [ new Image(), new Image() ]
+        preload[0].src = $.facebox.settings.closeImage
+        preload[1].src = $.facebox.settings.loadingImage
+
+        $('#facebox').find('.b:first, .bl').each(function() {
+          preload.push(new Image())
+          preload.slice(-1).src = $(this).css('background-image').replace(/url\((.+)\)/, '$1')
+        })
+
+        $('#facebox .close').click($.facebox.close)
+        $('#facebox .close_image').attr('src', $.facebox.settings.closeImage)
+      }
+
+      // getPageScroll() by quirksmode.com
+      function getPageScroll() {
+        var xScroll, yScroll;
+        if (self.pageYOffset) {
+          yScroll = self.pageYOffset;
+          xScroll = self.pageXOffset;
+        } else if (document.documentElement && document.documentElement.scrollTop) {     // Explorer 6 Strict
+          yScroll = document.documentElement.scrollTop;
+          xScroll = document.documentElement.scrollLeft;
+        } else if (document.body) {// all other Explorers
+          yScroll = document.body.scrollTop;
+          xScroll = document.body.scrollLeft;
+        }
+        return new Array(xScroll,yScroll)
+      }
+
+      // Adapted from getPageSize() by quirksmode.com
+      function getPageHeight() {
+        var windowHeight
+        if (self.innerHeight) { // all except Explorer
+          windowHeight = self.innerHeight;
+        } else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+          windowHeight = document.documentElement.clientHeight;
+        } else if (document.body) { // other Explorers
+          windowHeight = document.body.clientHeight;
+        }
+        return windowHeight
+      }
+
+      // Backwards compatibility
+      function makeCompatible() {
+        var $s = $.facebox.settings
+
+        $s.loadingImage = $s.loading_image || $s.loadingImage
+        $s.closeImage = $s.close_image || $s.closeImage
+        $s.imageTypes = $s.image_types || $s.imageTypes
+        $s.faceboxHtml = $s.facebox_html || $s.faceboxHtml
+      }
+
+      // Figures out what you want to display and displays it
+      // formats are:
+      //     div: #id
+      //   image: blah.extension
+      //    ajax: anything else
+      function fillFaceboxFromHref(href, klass) {
+        // div
+        if (href.match(/#/)) {
+          var url    = window.location.href.split('#')[0]
+          var target = href.replace(url,'')
+          if (target == '#') return
+          $.facebox.reveal($(target).html(), klass)
+
+        // image
+        } else if (href.match($.facebox.settings.imageTypesRegexp)) {
+          fillFaceboxFromImage(href, klass)
+        // ajax
+        } else {
+          fillFaceboxFromAjax(href, klass)
+        }
+      }
+
+      function fillFaceboxFromImage(href, klass) {
+        var image = new Image()
+        image.onload = function() {
+          $.facebox.reveal('<div class="image"><img src="' + image.src + '" /></div>', klass)
+        }
+        image.src = href
+      }
+
+      function fillFaceboxFromAjax(href, klass) {
+        $.get(href, function(data) { $.facebox.reveal(data, klass) })
+      }
+
+      function skipOverlay() {
+        return $.facebox.settings.overlay == false || $.facebox.settings.opacity === null
+      }
+
+      function showOverlay() {
+        if (skipOverlay()) return
+
+        if ($('#facebox_overlay').length == 0)
+          $("body").append('<div id="facebox_overlay" class="facebox_hide"></div>')
+
+        $('#facebox_overlay').hide().addClass("facebox_overlayBG")
+          .css('opacity', $.facebox.settings.opacity)
+          .click(function() { $(document).trigger('close.facebox') })
+          .fadeIn(200)
+        return false
+      }
+
+      function hideOverlay() {
+        if (skipOverlay()) return
+
+        $('#facebox_overlay').fadeOut(200, function(){
+          $("#facebox_overlay").removeClass("facebox_overlayBG")
+          $("#facebox_overlay").addClass("facebox_hide")
+          $("#facebox_overlay").remove()
+        })
+
+        return false
+      }
+
+      /*
+       * Bindings
+       */
+
+      $(document).bind('close.facebox', function() {
+        $(document).unbind('keydown.facebox')
+        $('#facebox').fadeOut(function() {
+          $('#facebox .content').removeClass().addClass('content')
+          $('#facebox .loading').remove()
+          $(document).trigger('afterClose.facebox')
+        })
+        hideOverlay()
+      })
+
+      bindFacebox();
+    }
+
+    function bindFacebox() {
+        var $iframe = jQuery('<iframe width="600px" height="390px" frameborder="0" scrolling="auto">Iframe not Supported</iframe>')
+                        .attr('src',  pandaSettings.apiUrl + '/' + pandaSettings.apiVrsn + '/testimonials/new.iframe?apikey='+pandaSettings.apikey);
+
+        $container.find("a.pandA-addForm_ness").click(function() {
+            jQuery.facebox($iframe);
+            return false;
+        });
+    }
+
+    return {
+        init: init,
+        display: display,
+        update: update
+    }
+}();
+
 window.onload = panda.init();
